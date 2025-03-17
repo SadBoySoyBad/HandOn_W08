0b341d06fc14cd52aa2f0aa39094428e64cbdb61
[SadBoySoyBad] <[your_email@domain.com]>
2025-03-17 15:45:22 +0700

    Adding new ecommerce app and item page

--- general/views.py
+++ general/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def ecommerce_index_view(request):
+    '''This function render index page of ecommerce view'''
+
+    return HttpResponse('Wellcome to CN334 kittipon View!')
